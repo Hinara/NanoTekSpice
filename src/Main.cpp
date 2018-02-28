1d668c114bf669ca587e1bcc5c1cbbc99c7a96fe
robbix1206 <robbix1206@gmail.com>
2018-02-28 13:43:28 +0100

    Reindent the makefile and add usage into the main

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,11 +5,16 @@
 ** main
 */
 
+#include <stdio.h>
 #include "Kernel.hpp"
 
 int	main(const int ac, const char **av)
 {
-	if (ac < 2)
+	if (ac < 2) {
+		std::cerr << "Bad number of arguments" << std::endl
+			<< "Usage: " << av[0] << " file.nts [input=1|0 ...]"
+			<< std::endl;
 		return 84;
+	}
 	return Kernel::run(ac, av);
 }
