2144716b82ccc9e8526ff0cf65acf1f29c968f5a
robbix1206 <robbix1206@gmail.com>
2018-03-02 20:50:54 +0100

    Add tests on supercomponent about loops, links, and get unliked pin val

--- tests/SuperComponentTests.cpp
+++ tests/SuperComponentTests.cpp
@@ -7,7 +7,8 @@
 
 #include "../src/Components/Output.hpp"
 #include "../src/Components/Input.hpp"
-#include "../src/Components/LogicGates.hpp"
+#include "../src/Components/Comp4071.hpp"
+#include "../src/Errors.hpp"
 #include <criterion/criterion.h>
 
 Test(misc, supercomponentisinputfalse)
@@ -36,4 +37,49 @@ Test(misc, supercomponentisoutputfalse)
 	Output			o("None");
 
 	cr_assert(o.isOutput(1) == false);
+}
+
+Test(misc, supercomponentloop)
+{
+	Input			i("None");
+	Comp4071		orComp("None");
+	Output			o("None");
+
+	orComp.setLink(1, i, 1);
+	orComp.setLink(2, orComp, 3);
+	o.setLink(1, orComp, 3);
+	i.setState(nts::FALSE);
+	o.simulate();
+	cr_assert(o.getValue() == nts::UNDEFINED);
+	i.setState(nts::TRUE);
+	o.simulate();
+	cr_assert(o.getValue() == nts::TRUE);
+	i.setState(nts::FALSE);
+	o.simulate();
+	cr_assert(o.getValue() == nts::TRUE);
+}
+
+Test(misc, supercomponentdoublelink)
+{
+	Input			i("None");
+	Output			o("None");
+
+	o.setLink(1, i, 1);
+	cr_assert_throw(o.setLink(1, i, 1), Err::LinkError);
+}
+
+Test(misc, supercomponentlinkunknown)
+{
+	Input			i("None");
+	Output			o("None");
+
+	cr_assert_throw(o.setLink(0, i, 1), Err::LinkError);
+}
+
+Test(misc, supercomponentgetinputnotlinked)
+{
+	Output			o("None");
+
+	o.simulate();
+	cr_assert(o.getValue() == nts::UNDEFINED);
 }
