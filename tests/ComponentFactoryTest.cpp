4b08ca73acdb57ba90e23d3a6791ac845c6d2803
robbix1206 <robbix1206@gmail.com>
2018-03-02 20:11:31 +0100

    Add test for all component into the component factory

--- tests/ComponentFactoryTest.cpp
+++ tests/ComponentFactoryTest.cpp
@@ -12,7 +12,25 @@ Test(misc, componentfactorysuccess)
 {
 	nts::ComponentFactory		cf;
 
-	cr_expect_no_throw(cf.createComponent("4001", "v"), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4001", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4008", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4011", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4013", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4017", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4030", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4040", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4069", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4071", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4081", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4094", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4514", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("4801", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("2716", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("input", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("output", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("clock", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("true", ""), std::out_of_range);
+	cr_expect_no_throw(cf.createComponent("false", ""), std::out_of_range);
 }
 
 Test(misc, componentfactoryerror)
